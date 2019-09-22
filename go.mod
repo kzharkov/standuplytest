e6ef7ab5c14afc34978ebf8e45385eb17cac59aa
iorweth <sir.k.zharkov@gmail.com>
2019-09-22 16:01:02 +0700

    Add order command

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/buaazp/fasthttprouter v0.1.1
+	github.com/nlopes/slack v0.6.0
 	github.com/qiangxue/fasthttp-routing v0.0.0-20160225050629-6ccdc2a18d87 // indirect
 	github.com/valyala/fasthttp v1.5.0
 )
--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"github.com/buaazp/fasthttprouter"
-	"github.com/valyala/fasthttp"
+	"encoding/json"
+	"github.com/nlopes/slack"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 func main() {
-	router := fasthttprouter.New()
+	apiKey := "xoxp-770274988487-770274988743-770642068102-6163d973a4d1d798cdf96bf0a01c8dc8"
+	router := http.NewServeMux()
 
-	router.POST("/order", Order)
+	router.HandleFunc("/order", Order(apiKey))
 
-	if err := fasthttp.ListenAndServe(":80", router.Handler); err != nil {
+	if err := http.ListenAndServe(":8443", router); err != nil {
 		log.Println(err)
 	}
 }
 
-func Order(ctx *fasthttp.RequestCtx) {
-	fmt.Println(ctx.PostBody())
+func Order(apiKey string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		verifier, err := slack.NewSecretsVerifier(r.Header, apiKey)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "InternalError", 500)
+		}
+		r.Body = ioutil.NopCloser(io.TeeReader(r.Body, &verifier))
+		s, err := slack.SlashCommandParse(r)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if err = verifier.Ensure(); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		params := &slack.Msg{Text: s.Text}
+		b, err := json.Marshal(params)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		_, err = w.Write(b)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
 }
